28f7ec3f85ae9c690682815711d3f58f047283d2
kotrbakristian-RepoSite <kotrba.kristian@gmail.com>
2026-01-16 11:36:31 +0100

    Spremno test

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,94 +10,85 @@ namespace WebApplication2.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-              
-
+        private readonly ILogger<IndexModel> _logger;   
         private readonly Models.ApplicationDbContext _context;
         private readonly IConfiguration Configuration;
+        public List<Rezultat> Customers { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, Models.ApplicationDbContext context, IConfiguration configuration)
         {
             _logger = logger;
             _context = context;
             Configuration = configuration;
-        }
-
-
-        public List<Rezultat> Customers { get; set; }
-
-        public void OnGetSomeStuff() 
-        {
-
-            var rows = from o in _context.Rezultatis
-                       select o;
-            foreach (var row in rows)
-            {
-                _context.Remove(row);
-            }
-            _context.SaveChanges();
-
-
-
-
-            this.Customers = _context.Rezultatis.ToList();
-            
-
-            for (int i = 1; i < 10; i++)
-            { 
-
-            var customSearchService = new Google.Apis.CustomSearchAPI.v1.CustomSearchAPIService(new BaseClientService.Initializer
-            {
-                ApiKey = "AIzaSyD24f7MpzEmRs0rO8d4bBKs2-DfGAzyYRg"
-            });
-            
-
-            var listRequest = customSearchService.Cse.List();
-            listRequest.Q = "Sekom";
-            listRequest.Cx = "833c9c97e2643439c";
-
-            
-            listRequest.Num = 10;
-            listRequest.Start = i * 10;
-            
-            var search = listRequest.Execute();
-                if (search.Items != null)
-                {
-                    
-
-                    foreach (var result in search.Items)
-                    {
-                        Rezultat re = new Rezultat() { Naslov = result.HtmlTitle, Html = result.HtmlSnippet };
-
-                        _context.Add(re);
-                        _context.SaveChanges();
-
-
-
-                    }
-
-                }
-
-
-
-            }
-
-            //var request = HttpContext.Request;
-            //var _baseURL = $"{request.Scheme}://{request.Host}";
-            //string fullUrl = _baseURL + HttpContext.Request.Path + HttpContext.Request.QueryString;
-            OnGet();
-        }
-
-
-
-
+        }   
         
 
-            public void OnGet()
+        public void OnPost()
         {
 
-            this.Customers = _context.Rezultatis.ToList();
-            
+
+
+            if (Request.Form["pret"] == "Pretraži pojam")
+            {
+
+
+
+                string pojam = Request.Form["val"];
+
+                //Ciscenje starih rows iz baze
+                _context.Database.ExecuteSqlRaw("TRUNCATE TABLE [Rezultati]");
+
+                this.Customers = _context.Rezultatis.ToList();
+
+
+                //Google search 100 rezultata i upis u bazu
+                for (int i = 1; i < 10; i++)
+                {
+
+                    var customSearchService = new Google.Apis.CustomSearchAPI.v1.CustomSearchAPIService(new BaseClientService.Initializer
+                    {
+                        ApiKey = "AIzaSyD24f7MpzEmRs0rO8d4bBKs2-DfGAzyYRg"
+                    });
+
+
+                    var listRequest = customSearchService.Cse.List();
+                    listRequest.Q = pojam;
+                    listRequest.Cx = "833c9c97e2643439c";
+                    listRequest.Num = 10;
+                    listRequest.Start = i * 10;
+
+                    var search = listRequest.Execute();
+                    if (search.Items != null)
+                    {
+                        foreach (var result in search.Items)
+                        {
+                            Rezultat re = new Rezultat() { Naslov = result.HtmlTitle, Html = result.HtmlSnippet };
+                            _context.Add(re);
+                            _context.SaveChanges();
+                        }
+                    }
+                }
+                OnGet();
+
+
+
+            }
+            else if (Request.Form["pretF"] == "Filter pojam")
+            {
+
+                string pojam = Request.Form["valF"];
+                this.Customers = _context.Rezultatis.Where(p => p.Naslov.Contains(pojam) || p.Html.Contains(pojam)).ToList();
+                               
+            }
+                   
         }
+
+
+        public void OnGet()
+        {                      
+            if(_context.Rezultatis.Any())
+            this.Customers = _context.Rezultatis.ToList();            
+        }
+
     }
 }
